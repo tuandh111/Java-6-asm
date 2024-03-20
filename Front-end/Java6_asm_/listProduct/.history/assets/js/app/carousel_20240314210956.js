31eece40ae1cd3220f419d8a6c0310d09d5754f7
TrungAug <trung2894@gmail.com>
2024-03-20 16:20:04 +0700

    update new prjoect

--- Front-end/Java6_asm_/listProduct/.history/assets/js/app/carousel_20240314210956.js
+++
@@ -1,7 +0,0 @@
-let nextCarouselDOM = document.createElementById('next-carousel');
-let prevCarouselDOM = document.createElementById('prev-carousel');
-let carouselDOM = document.querySelector('.carousel')
-let listCarouselDOM = document.querySelector('.carousel .list-carousel')
-let thumnailDOM = document.querySelector('.carousel .thumnails')
-
-nextCarouselDOM.onClick
