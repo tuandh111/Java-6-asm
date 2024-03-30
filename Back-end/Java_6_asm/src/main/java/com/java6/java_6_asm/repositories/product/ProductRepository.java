efd8b8456927cfc71364e7095c13cdb2ac328c0c
TrungAug <trung2894@gmail.com>
2024-03-30 11:45:57 +0700

    FE done homepage, fix product card, header, footer

--- Back-end/Java_6_asm/src/main/java/com/java6/java_6_asm/repositories/product/ProductRepository.java
+++ Back-end/Java_6_asm/src/main/java/com/java6/java_6_asm/repositories/product/ProductRepository.java
@@ -11,12 +11,15 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
-    @Query("SELECT p, b.nameBrand, " +
+    @Query("SELECT p, b.nameBrand, d.discountedPrice, " +
             "(SELECT STRING_AGG(i.imageName, ',') FROM ProductImage i WHERE i.product = p), " +
             "(SELECT STRING_AGG(s.sizeName, ',') FROM DetailsSize ds JOIN ds.product dp JOIN ds.size s WHERE dp = p), " +
             "(SELECT STRING_AGG(c.colorName, ',') FROM DetailsColor dc JOIN dc.product dp JOIN dc.color c WHERE dp = p) " +
             "FROM Product p " +
-            "JOIN p.brand b where p.isActive=true and p.quantityInStock>0 order by p.createDate desc")
+            "JOIN p.brand b " +
+            "JOIN Discount d ON d.product=p " +
+            "where p.isActive=true and p.quantityInStock>0 "+
+            "order by p.createDate desc")
     List<Object[]> findAllProduct();
 
     @Query("select c.product,(SELECT STRING_AGG(i.imageName, ',') FROM ProductImage i WHERE i.product = c.product) from Order o Join o.cart c Join c.product where o.status like '%Thành công%' group by c.product "
